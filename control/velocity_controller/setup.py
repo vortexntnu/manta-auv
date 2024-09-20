8640b74d6a6e2158af2c6a02b647ae3273abdde6
Cyprian <cyprian.github@gmail.com>
2024-09-20 17:48:13 +0200

    feat: made publisher node with correct topic name and variables

--- control/velocity_controller/setup.py
+++ control/velocity_controller/setup.py
@@ -7,8 +7,7 @@ setup(
     version="0.0.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
-        ("share/ament_index/resource_index/packages",
-         ["resource/" + package_name]),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
     ],
     install_requires=["setuptools"],
@@ -20,7 +19,7 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
-            "velocity_controller = control/velocity_controller.velocity_controller:main"
+            "velocity_controller = velocity_controller.velocity_controller:main"
         ],
     },
 )
--- control/velocity_controller/velocity_controller/velocity_controller.py
+++ control/velocity_controller/velocity_controller/velocity_controller.py
@@ -1,5 +1,6 @@
 #!usr/bin/env/python 3
 import rclpy
+from geometry_msgs.msg import Wrench
 from nav_msgs.msg import Odometry
 from rclpy.node import Node
 
@@ -8,20 +9,28 @@ class VelocityNode(Node):
 
     def __init__(self):
         super().__init__("input_subscriber")
-        self.topic_subscriber = self.create_subscription(
-            Odometry, "/nucleus/odom", self.subscribe_callback, 10)
+        self.topic_subscriber = self.create_subscription(Odometry, "/nucleus/odom", self.subscribe_callback, 10)
+        self.publisher = self.create_publisher(Wrench, "/thrust/wrench_input", 10)
+        self.timer = self.create_timer(0.1, self.publish_callback)
+        
 
     def subscribe_callback(self, msg: Odometry):
         self.get_logger().info(
-            f"position_x: {msg.pose.pose.position.x}\n_y: {msg.pose.pose.position.y}\n_z: {msg.pose.pose.position.z}"
+            f"\n_x: {msg.pose.pose.position.x}\n_y: {msg.pose.pose.position.y}\n_z: {msg.pose.pose.position.z}"
         )
+    
+    def publish_callback(self):
+        msg = Wrench()
+        msg.force.x = 1.0
+        msg.torque.y = 1.0
+        msg.torque.z = 1.0
+        self.publisher.publish(msg)
 
 
 def main(args=None):
     rclpy.init(args=args)
     node = VelocityNode()
     rclpy.spin(node)
-
     rclpy.shutdown()
 
 
