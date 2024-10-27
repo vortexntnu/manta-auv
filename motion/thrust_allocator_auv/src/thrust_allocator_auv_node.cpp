1146f846dd8da4b09cd8d6b086ac990b7b8f7350
Andreas Kluge Svendsrud <89779148+kluge7@users.noreply.github.com>
2024-10-27 22:15:57 +0100

    testing

--- motion/thrust_allocator_auv/src/thrust_allocator_auv_node.cpp
+++ motion/thrust_allocator_auv/src/thrust_allocator_auv_node.cpp
@@ -1,11 +1,24 @@
-#include "thrust_allocator_auv/thrust_allocator_ros.hpp"
-#include "thrust_allocator_auv/thrust_allocator_utils.hpp"
+#include "thruster_interface_auv/thruster_interface_auv_ros.hpp"
+#include <vector>
+#include <iostream>
 
-int main(int argc, char **argv) {
-  rclcpp::init(argc, argv);
-  auto allocator = std::make_shared<ThrustAllocator>();
-  RCLCPP_INFO(allocator->get_logger(), "Thrust allocator initiated");
-  rclcpp::spin(allocator);
-  rclcpp::shutdown();
-  return 0;
+int main(int argc, char** argv) {
+    rclcpp::init(argc, argv);
+
+    // Issue 1: Inefficient logging - using std::string instead of a string literal
+    std::string log_message = "Started thruster_interface_auv_node";  // Unnecessary allocation
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), log_message.c_str());
+
+    // Issue 2: Implicit casting from int to float
+    int update_rate = 10;
+    float update_period = 1.0 / update_rate;  // Implicit narrowing cast
+
+    // Issue 3: Unused variable - clang-tidy will warn if a variable is unused
+    std::vector<int> unused_vector = {1, 2, 3};  // Unused variable
+
+    // Issue 4: Avoid creating shared_ptr in function arguments - inefficient and can lead to multiple instances
+    rclcpp::spin(std::make_shared<ThrusterInterfaceAUVNode>());
+
+    rclcpp::shutdown();
+    return 0;
 }
